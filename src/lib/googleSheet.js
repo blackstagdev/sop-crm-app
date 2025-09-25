98523bdcb933879e0d0d86d7b466932fc7b29b3c
icbl <albert@blackstag.us>
2025-09-25 18:01:53 +0800

    change google auth

--- src/lib/googleSheet.js
+++ src/lib/googleSheet.js
@@ -4,7 +4,7 @@ import { google } from "googleapis";
 
 const jwt = new google.auth.JWT({
   email: process.env.GOOGLE_CLIENT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY,
+  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
